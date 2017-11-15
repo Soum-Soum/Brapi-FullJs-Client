c6f7ee825f0692be9b4c8fc178319961176a4c68
Unknown <pes.carceller@gmail.com>
2017-11-15 15:59:13 +0100

    tsv Génération 2/2

--- js/export.js
+++ js/export.js
@@ -2,7 +2,6 @@ async function ExportDetailsGermplasms(){
     let jsonHmap = [],  argumentsArray;
     let selectedGermplasms = $("#Germplasms option:selected").map(function(){return $(this).text().split(",");}).get();
     let germplasmId;
-    let finalHmap = [];
     selectedGermplasms = removeAll(selectedGermplasms, "");
     console.log(selectedGermplasms);
     for(let i=0; i<selectedGermplasms.length; i++){
@@ -15,7 +14,7 @@ async function ExportDetailsGermplasms(){
     let fieldTab = getFieldFormJson(jsonHmap);
     console.log(fieldTab);
     let tsvString = buildTsvString(jsonHmap, selectedGermplasms, fieldTab);
-    download('GermplasmsDetails.tsv',jsonHmap,tsvString);
+    download('GermplasmsDetails.tsv',tsvString);
 }
 
 function getFieldFormJson(HMap){
@@ -23,28 +22,50 @@ function getFieldFormJson(HMap){
     Object.keys(HMap).forEach(function(element){
         for(let key in HMap[element]){
             if(!isInArray(fieldTab,key) && key !== 'germplasmDbId'){
-                fieldTab.push(key);
+                //fieldTab.push(key);
+                if(HMap[element][key]!==null && HMap[element][key]!==undefined && HMap[element][key]!==''){
+                    fieldTab[key]=true;
+                }else{
+                    fieldTab[key]=false;
+                }
+            }else{
+                if(fieldTab[key]===false && (HMap[element][key]!==null && HMap[element][key]!==undefined && HMap[element][key]!=='')){
+                    fieldTab[key]=true;
+                }
             }
         }
     });
+    console.log(fieldTab);
     return fieldTab;
 }
 
 function buildTsvString(jsonHamp, selectedGermplasms, fieldTab){
-    let tsvString = '';
-    fieldTab.forEach(function (element){
-       tsvString += element + '\t';
+    let tsvString = 'germplasmDbId \tmarkerProfileDbId \t';
+    Object.keys(fieldTab).forEach(function (element){
+       if(fieldTab[element]===true){
+           console.log(element);
+           tsvString += element + '\t';
+       }
     });
     tsvString+='\n';
     selectedGermplasms.forEach(function (element){
-        cpyResp[element].forEach(function(element2){
-            tsvString+=(element2.germplasmDbId + "\t" + element2.markerProfileDbId + "\t");
-        });
-        fieldTab.forEach(function (element2){
-            if(jsonHamp[element][element2]!==null && jsonHamp[element][element2]!== undefined){
-                tsvString+= jsonHamp[element][element2] + '\t'
-            }else{
-                tsvString+='\t';
+        console.log(cpyResp);
+        tsvString+=cpyResp[element][0].germplasmDbId + "\t";
+        if(cpyResp[element].length===1){
+            tsvString+=cpyResp[element][0].markerProfileDbId;
+        }else{
+            cpyResp[element].forEach(function(element2){
+                tsvString+=element2.markerProfileDbId + ' ; ';
+            });
+        }
+        tsvString+='\t';
+        Object.keys(fieldTab).forEach(function (element2){
+            if(fieldTab[element2]===true){
+                if(jsonHamp[element][element2]!==null && jsonHamp[element][element2]!== undefined){
+                    tsvString+= jsonHamp[element][element2] + '\t'
+                }else{
+                    tsvString+='\t';
+                }
             }
         });
         tsvString+='\n';
@@ -53,9 +74,8 @@ function buildTsvString(jsonHamp, selectedGermplasms, fieldTab){
     return tsvString;
 }
 
-function download(filename,hmap, tsvData) {
+function download(filename, tsvData) {
     let element = document.createElement('a');
-    let text;
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(tsvData));
     element.setAttribute('download', filename);
     element.style.display = 'none';
